ba0f8aa6e2db1c792754b05664801e9fdccefb8f
Василий Сатанцев <30374628+SkyRanger2010@users.noreply.github.com>
2025-09-14 08:51:12 +0300

    task3 read 2 numbers in line, outpu sum & diff in line

---
+++ task3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a + b;
+        int diff = a - b;
+        System.out.println(sum + " " + diff);
+    }
+}
