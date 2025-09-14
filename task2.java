2d2a99cfafbfda1f9f91f355216719bac88c0f9e
Василий Сатанцев <30374628+SkyRanger2010@users.noreply.github.com>
2025-09-14 08:49:32 +0300

    Task2 Cut 3 digits after comma

---
+++ task2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        int x3 = (int) (x * 1000);
+        double cut = x3 / 1000.0;
+        System.out.println(cut);
+    }
+}
